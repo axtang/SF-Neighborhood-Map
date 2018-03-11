69b8fc2631aa69182852033ee00b6cc479926b81
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-11 15:32:28 -0700

    Update app.js
    Adding google OAuth.

--- js/app.js
+++ js/app.js
@@ -96,10 +96,51 @@ var octopus = function() {
 		});
 	}
 
-	
+
 	function googleOAuth(){
+		function handleClientLoad() {
+			gapi.load('client:auth2', initClient);
+		}
 
+		function initClient() {
+			gapi.client.init({
+			    apiKey: 'AIzaSyDtyj0R3_0LUIv8Z155h3GK3FiwQKt6858',
+			    discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+			    clientId: 'sf-neighborhood-map.apps.googleusercontent.com',
+			    scope: 'profile'
+			}).then(function () {
+			    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
+			    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+			});
+		}
+
+		function updateSigninStatus(isSignedIn) {
+			if (isSignedIn) {
+				makeApiCall();
+			}
+		}
+
+		function handleSignInClick(event) {
+			gapi.auth2.getAuthInstance().signIn();
+		}
+
+		function handleSignOutClick(event) {
+			gapi.auth2.getAuthInstance().signOut();
+			}
+
+		function makeApiCall() {
+			gapi.client.people.people.get({
+				'resourceName': 'people/me',
+				'requestMask.includeField': 'person.names'
+			}).then(function(response) {
+				console.log(
+					'Hello, ' + response.result.names[0].givenName);
+			}, function(reason) {
+				console.log(
+					'Error: ' + reason.result.error.message);
+			});
+		}
 	}
 
 
