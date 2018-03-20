9f0a908b543b90c7798d9f39a4adb4680330001c
Annabelle Tang <tangannabelle1@gmail.com>
2018-03-20 14:36:05 -0700

    Update app.js
    Adding initAutocomplete().

--- js/app.js
+++ js/app.js
@@ -390,6 +390,70 @@ function clickedMarker(name) {
 }
 
 
+// Autocomplete the search phrase in the search box.
+function initAutocomplete() {
+	// creat the search box and link it to the UI element
+	var input = document.getElementById('pac-input');
+	var searchBox = new google.maps.locations.SearchBox(input);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+	// bias the search box results towards the current map's viewport
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	var markers = [];
+	
+	// listen for the event fired when the user selects a prediction
+	// and retrieve more details for that place
+	searchBox.addListener('places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+			return;
+		}
+
+		// clear out old markers
+		markers.forEach(function(marker){
+			marker.setmap(null);
+		});
+		markers = [];
+
+		// for each place, get the icon, name and location
+		var bounds = new google.maps.LatLngBounds();
+		places.forEach(function(place){
+			if (!place.geometry) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
+			var icon = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25)
+			};
+
+			// create a marker for each place
+			markers.push(new google.maps.Marker({
+				map: map,
+				icon: icon,
+				title: place.name,
+				position: place.geometry.location
+			}));
+
+			if (place.geometry.viewport) {
+				// only geocodes have viewport
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});	
+}
+
+
 // Use Yelp Search API to get information on each location.
 function yelpSearch(location) {
 	var search_url = "https://api.yelp.com/v3/businesses/search"
